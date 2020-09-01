4a6bc768e6bd71f401a89ff8f8086c268a146020
shotaro-kobayashi <shotaro.kobayashi.9x@stu.hosei.ac.jp>
2020-09-01 16:58:39 +0900

    結合テストの実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -24,7 +24,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids:[])
+    params.require(:room).permit(:name, user_ids:[ ])
   end
 
 end
